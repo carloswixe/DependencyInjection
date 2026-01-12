9ed8fb91ea53dd5ac065343b7454d53b8c303479
Carlos Franco <carlos.franco.pkt1@hotmail.com>
2026-01-12 13:51:40 -0700

    CAFR: CARGA INICIAL

---
+++ OCRTesseract/OCRTesseract/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace OCRTesseract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string pathImage = @"./LICENSE_FRONT.jpeg";
+            string path = System.IO.Path.GetFullPath(@"./");
+            string text = "";
+            try
+            {
+                using (var engine = new Tesseract.TesseractEngine(@"./tessdata", "spa", Tesseract.EngineMode.TesseractAndLstm))
+                {
+                    using (var img = Tesseract.Pix.LoadFromFile(pathImage))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            text = page.GetText();
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+                        }
+                    }
+                }
+
+            }
+            catch (Exception Ex)
+            {                
+                Console.WriteLine(Ex.Message);
+            }
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
